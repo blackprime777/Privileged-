ede57cda02962935642aca3afdb0c7ddc3cc00b2
payback <freedomdaniel989i@gmail.com>
2026-07-03 16:31:27 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "Kali Web Simulator",
+  description: "Realistic Kali Linux Terminal",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
